c2804152d91cef8e02ba088bc782a2de6b1b2c26
user <user@DESKTOP-D4E3G0Q>
2023-05-21 22:26:19 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.cmbComport = new System.Windows.Forms.ToolStripComboBox();
+            this.cmbBoardlate = new System.Windows.Forms.ToolStripComboBox();
+            this.btnConnect = new System.Windows.Forms.ToolStripButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmbComport,
+            this.cmbBoardlate,
+            this.btnConnect});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(405, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // cmbComport
+            // 
+            this.cmbComport.Name = "cmbComport";
+            this.cmbComport.Size = new System.Drawing.Size(121, 25);
+            // 
+            // cmbBoardlate
+            // 
+            this.cmbBoardlate.Name = "cmbBoardlate";
+            this.cmbBoardlate.Size = new System.Drawing.Size(121, 25);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnConnect.Image = ((System.Drawing.Image)(resources.GetObject("btnConnect.Image")));
+            this.btnConnect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(56, 22);
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "체감온도";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "온도";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "습도";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(237, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "데이터";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 143);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 339);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox cmbComport;
+        private System.Windows.Forms.ToolStripComboBox cmbBoardlate;
+        private System.Windows.Forms.ToolStripButton btnConnect;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort comport = new SerialPort();
+        private delegate void SetTextDelegate(string getString);
+        public Form1()
+        {
+            InitializeComponent();
+            comport.DataReceived += new SerialDataReceivedEventHandler(DataRecieved);
+
+        }
+        private void DataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            string rxd = comport.ReadTo("\n");
+            this.BeginInvoke(new SetTextDelegate(SerialReceived), new object[] { rxd });
+        }
+
+        private void SerialReceived(string inString)
+        {
+            textBox1.AppendText(inString + "\r\n");
+            string[] data = inString.Split(',');
+
+
+
+            label2.Text = "습도 : " + data[0] + "%";
+            label3.Text = "온도 : " + data[1] + "°C";
+            //label4.Text = "체감온도 : " + data[2] + "'C";
+            label4.Text = "체감온도 : " + data[2].Replace("\n", "").Replace("\n", "") + "°C".Replace("\n", "");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbComport.Items.Clear();
+            var portName = System.IO.Ports.SerialPort.GetPortNames();
+            cmbComport.Items.AddRange(portName);
+            cmbComport.SelectedIndex = cmbComport.Items.Count - 1;
+
+            cmbBoardlate.Items.Clear();
+            cmbBoardlate.Items.Add("9600");
+            cmbBoardlate.Items.Add("115200");
+            cmbBoardlate.SelectedIndex = 0;
+
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "Connect")
+            {
+                comport.PortName = cmbComport.Text;
+                comport.BaudRate = Convert.ToInt16(cmbBoardlate.Text);
+                comport.DataBits = 8;
+                comport.Parity = Parity.None;
+                comport.StopBits = StopBits.One;
+                comport.Handshake = Handshake.None;
+                comport.Open();
+                comport.DiscardInBuffer();
+                btnConnect.Text = "Close";
+
+
+            }
+            else
+            {
+                comport.Close();
+                btnConnect.Text = "Connect";
+            }
+        }
+    }
+}
